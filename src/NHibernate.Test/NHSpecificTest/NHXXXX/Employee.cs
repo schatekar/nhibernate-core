c3dfd7915977bf9cc8a69846634f7d95a6e878c2
Suhas Chatekar <suhas.chatekar@collinsontech.com>
2014-12-12 16:59:13 +0000

    Setting up NH development environment locally

---
+++ src/NHibernate.Test/NHSpecificTest/NHXXXX/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace NHibernate.Test.NHSpecificTest.NHXXXX
+{
+    public class Employee
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; } 
+    }
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NHXXXX/NHXXXXFixture.cs
@@ -0,0 +1,37 @@
+﻿using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NHXXXX
+{
+    [TestFixture]
+    public class NHXXXXFixture : BugTestCase
+    {
+        [Test]
+        public void MapsPrimitiveProperties()
+        {
+            using (var session = OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var id = session.Save(new Employee
+                    {
+                        Name = "John"
+                    });
+                    transaction.Commit();
+                }
+            }
+        }
+
+        protected override void Configure(Cfg.Configuration configuration)
+        {
+            base.Configure(configuration);
+            configuration.DataBaseIntegration(db =>
+            {
+                db.Dialect<SQLiteDialect>();
+                db.Driver<SQLite20Driver>();
+            });
+        }
+    }
+}
